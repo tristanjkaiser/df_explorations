0e7cf9b45f57432d29896de374250ba0b273a0c0
Antonio Skilton <antonio.skilton@little-bamboo.com>
2017-02-07 18:57:34 -0800

    done!

--- df_explorations.R
+++ df_explorations.R
@@ -1,27 +1,19 @@
 # df_explorations
-# By: Tristan Kaiser
+# By: Tristan Kaiser & Antonio Skilton
 # Last edited: 2/7/2017
 
-library(tidyverse)
+# start with categorical variable
+df <- data.frame(letter = sample(letters[1:5], 100, TRUE))
 
-# Create data frame
-df <- data.frame(matrix(0, nrow = 100, ncol = 6))
-names(df) <- c("letter", "a", "b", "c", "d", "e")
-df$letter <- sample(letters[1:5], 100, TRUE)
-
-# This works to apply a 1 in the appropriate column given the value of df$letter
-for(i in seq_along(df$letter)){
-  df[i, df$letter[[i]]] <- 1
+fill_function <- function(df, column_name){
+  unique_values <- unique(df[, column_name])[order(unique(df[, column_name]))]
+  mx <- matrix(nrow = nrow(df), ncol = length(unique_values))
+  for(i in 1:length(unique_values)){
+    mx[,i] <- ifelse(df[, "letter"] == unique_values[i], 1, 0)
+  }
+  colnames(mx) <- unique_values
+  final_df <- cbind(df, mx)
+  return(final_df)
 }
 
-
-#### Function attempt ####
-# This doesn't work, but what I'm thinking about
-
-fill_function <- function(df, variable){
-    df[,variable] <- 1
-}
-
-df2 <- dmap(df, 1, fill_function(df, df$letter))
-
-df3 <- lapply(df, fill_function(df, df$letter))
+fill_function(df, "letter")
